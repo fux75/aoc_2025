52d2d5f0ab20ae2a1ff5e7f7c555db68d30dd121
Matthias Fuchs <m.fuchs@rhc-edv.de>
2025-12-02 07:39:34 +0100

    Umstrukturiert

---
+++ Day1-Puzzle2/PuzzleLock.cs
@@ -0,0 +1,59 @@
+﻿public class PuzzleLock
+{
+    public int CurrentPosition { get; set; }
+
+    public int TurnLeft(int aCount)
+    {
+        var aEffectiveCount = aCount % 100;
+        var aPassedZeros = aCount / 100;
+
+        var aCurrentPosition = CurrentPosition;
+        aCurrentPosition -= aEffectiveCount;
+        if ((aCurrentPosition <= 0) && (CurrentPosition != 0))
+            aPassedZeros++;
+
+        aCurrentPosition = (aCurrentPosition + 100) % 100;
+
+        CurrentPosition = aCurrentPosition;
+        return aPassedZeros;
+    }
+
+    public int TurnRight(int aCount)
+    {
+        var aEffectiveCount = aCount % 100;
+        var aPassedZeros = (aCount / 100);
+
+        var aCurrentPosition = CurrentPosition;
+        aCurrentPosition += aEffectiveCount;
+        if ((aCurrentPosition >= 100) && (CurrentPosition != 0))
+            aPassedZeros++;
+
+        aCurrentPosition %= 100;
+
+        CurrentPosition = aCurrentPosition;
+        return aPassedZeros;
+    }
+
+    public int Turn(string aCode)
+    {
+        var aDirection = aCode[0];
+        var aTicksString = aCode.Substring(1, (aCode.Length - 1));
+
+        if (!int.TryParse(aTicksString, out var aTicks))
+            throw new ArgumentException("Wrong direction");
+
+        if (aDirection == 'L')
+        {
+            return TurnLeft(aTicks);
+        }
+        else if (aDirection == 'R')
+        {
+            return TurnRight(aTicks);
+        }
+        else
+        {
+            throw new ArgumentException("Wrong direction");
+        }
+    }
+}
+
