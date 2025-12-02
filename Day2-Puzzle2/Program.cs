4a27c89abef99a2667c36feb1d55d1eb3d56ea9f
Matthias Fuchs <m.fuchs@rhc-edv.de>
2025-12-02 08:33:20 +0100

    Tag 2

---
+++ Day2-Puzzle2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+var aInput = File.ReadAllText("ProductIds.txt");
+var aRegEx = new Regex(@"^(\d+)\1+$");
+
+var aParts = aInput.Split(',');
+
+ulong aSum = 0;
+foreach (var aRange in aParts)
+{
+    var aFirstLast = aRange.Split('-');
+
+    ulong aFirstId = ulong.Parse(aFirstLast[0]);
+    ulong aLastId = ulong.Parse(aFirstLast[1]);
+
+    for (var aCheckId = aFirstId; aCheckId <= aLastId; aCheckId++)
+    {
+        var aCheckString = aCheckId.ToString();
+        if (aRegEx.IsMatch(aCheckString))
+            aSum += aCheckId;
+    }
+}
+
+Console.WriteLine($"Sum: {aSum}");
+Console.WriteLine("[ENTER]");
+Console.ReadLine();
