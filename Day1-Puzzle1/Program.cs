22bea03415685c93534d6d62779fb296a88af94c
Matthias Fuchs <m.fuchs@rhc-edv.de>
2025-12-02 07:33:21 +0100

    Projektdateien hinzufügen.

---
+++ Day1-Puzzle1/Program.cs
@@ -0,0 +1,16 @@
+﻿var aLock = new PuzzleLock();
+aLock.CurrentPosition = 50;
+var aZeros = 0;
+
+var aKeyCodes = File.ReadAllLines("Codes.txt");
+
+foreach (var aKeyCode in aKeyCodes)
+{
+    if (aLock.Turn(aKeyCode) == 0)
+        aZeros++;
+}
+
+Console.WriteLine($"The code is: {aZeros}");
+Console.WriteLine("[ENTER]");
+Console.ReadLine();
+
---
+++ Day1-Puzzle1/PuzzleLock.cs
@@ -0,0 +1,48 @@
+﻿public class PuzzleLock
+{
+    public int CurrentPosition { get; set; }
+
+    public int TurnLeft(int aCount)
+    {
+        var aCurrentPosition = CurrentPosition;
+        aCurrentPosition -= aCount;
+        aCurrentPosition = (aCurrentPosition + 100) % 100;
+
+        CurrentPosition = aCurrentPosition;
+        return CurrentPosition;
+    }
+
+    public int TurnRight(int aCount)
+    {
+        var aCurrentPosition = CurrentPosition;
+        aCurrentPosition += aCount;
+        aCurrentPosition %= 100;
+
+        CurrentPosition = aCurrentPosition;
+        return CurrentPosition;
+    }
+
+    public int Turn(string aCode)
+    {
+        var aDirection = aCode[0];
+        var aTicksString = aCode.Replace("L", "").Replace("R", "");
+
+        if (!int.TryParse(aTicksString, out var aTicks))
+            throw new ArgumentException("Wrong direction");
+
+        if (aDirection == 'L')
+        {
+            TurnLeft(aTicks);
+        }
+        else if (aDirection == 'R')
+        {
+            TurnRight(aTicks);
+        }
+        else
+        {
+            throw new ArgumentException("Wrong direction");
+        }
+
+        return CurrentPosition;
+    }
+}
