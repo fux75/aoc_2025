a7921ce682ea365f883cc05d0fe8823c334f79b9
Matthias Fuchs <m.fuchs@rhc-edv.de>
2025-12-04 13:43:17 +0100

    Kommentare für Puzzle 2 eingefügt

--- Day4-Puzzle1/Program.cs
+++ Day4-Puzzle1/Program.cs
@@ -1,43 +1,8 @@
 ﻿var aInput = File.ReadAllLines("input.txt").ToList();
 
-//List<string> aInput = [
-//    "..@@.@@@@.",
-//    "@@@.@.@.@@",
-//    "@@@@@.@.@@",
-//    "@.@@@@..@.",
-//    "@@.@@@@.@@",
-//    ".@@@@@@@.@",
-//    ".@.@.@.@@@",
-//    "@.@@@.@@@@",
-//    ".@@@@@@@@.",
-//    "@.@.@@@.@."
-//    ];
-
-
 var aPaperGrid = new PaperGrid(aInput);
+var aAccessibleRolls = aPaperGrid.MarkAccessibles();
 
-//for(var aRowIndex=0;aRowIndex < aPaperGrid.RowCount; aRowIndex++)
-//{
-//    for(var aColumnIndex=0;aColumnIndex < aPaperGrid.ColumnCount; aColumnIndex++)
-//    {
-//        Console.Write(aPaperGrid[aColumnIndex, aRowIndex]);
-//    }
-//    Console.WriteLine();
-//}
-
-//Console.WriteLine();
-var aAccessibleCount = aPaperGrid.MarkAccessibles();
-Console.WriteLine($"Accessible rolls: {aAccessibleCount}");
-//Console.WriteLine();
-
-//for (var aRowIndex = 0; aRowIndex < aPaperGrid.RowCount; aRowIndex++)
-//{
-//    for (var aColumnIndex = 0; aColumnIndex < aPaperGrid.ColumnCount; aColumnIndex++)
-//    {
-//        Console.Write(aPaperGrid[aColumnIndex, aRowIndex]);
-//    }
-//    Console.WriteLine();
-//}
-
+Console.WriteLine($"Accessible rolls: {aAccessibleRolls}");
 Console.WriteLine("[ENTER]");
 Console.ReadLine();
--- Day4-Puzzle2/PaperGrid.cs
+++ Day4-Puzzle2/PaperGrid.cs
@@ -1,21 +1,25 @@
 ﻿public class PaperGrid
 {
+    // Internest Grid, was eine leere Zeile oben und unten sowie eine leere Spalte vorne und hinten hat
+    // Spart den Krampf mit Grid Rändern
     private char[,] InternalGrid { get; set; } = new Char[0, 0];
     private int InternalColumnCount => InternalGrid.GetUpperBound(0) + 1;
     private int InternalRowCount => InternalGrid.GetUpperBound(1) + 1;
 
+    // Für eventuelle für DEBUG Prints
     public char this[int aColumn, int aRow]
     {
         get
         {
+            // Mapping auf interne Koordinaten
             return InternalGrid[aColumn + 1, aRow + 1];
         }
         set
         {
+            // Mapping auf interne Koordinaten
             InternalGrid[aColumn + 1, aRow + 1] = value;
         }
     }
-
     public int ColumnCount { get; private set; } = 0;
     public int RowCount { get; private set; } = 0;
 
@@ -25,6 +29,7 @@
         RowCount = aInput.Count;
         InternalGrid = new char[ColumnCount + 2, RowCount + 2];
 
+        // alles auf '.'
         for (var aRowIndex = 0; aRowIndex < InternalRowCount; aRowIndex++)
         {
             for (var aColumnIndex = 0; aColumnIndex < InternalColumnCount; aColumnIndex++)
@@ -33,6 +38,7 @@
             }
         }
 
+        // Nutzdaten setzen
         for (var aRowIndex = 0; aRowIndex < aInput.Count; aRowIndex++)
         {
             var aLine = aInput[aRowIndex];
@@ -60,6 +66,7 @@
     public int MarkAccessibles()
     {
         var Result = 0;
+        // 1. und letzte Spalte / Zeile weglassen (sind nur dummies)
         for (var aRowIndex = 1; aRowIndex < InternalRowCount - 1; aRowIndex++)
         {
             for (var aColumnIndex = 1; aColumnIndex < InternalColumnCount - 1; aColumnIndex++)
