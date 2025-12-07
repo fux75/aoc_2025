3dc4dab03267da46dc651874110942e0fd3424a2
Matthias Fuchs <m.fuchs@rhc-edv.de>
2025-12-07 20:44:51 +0100

    Day7 - Puzzle2

---
+++ Day7-Puzzle1/Program.cs
@@ -0,0 +1,54 @@
+﻿var aInput = File.ReadLines("Input.txt").ToList().Where(aLine => aLine.Any(s => s == 'S' || s == '^')).ToList();
+// var aInput = File.ReadLines("Sample.txt").ToList().Where(aLine => aLine.Any(s => s == 'S' || s == '^')).ToList();
+
+
+var aBeamColumns = new List<int>();
+aBeamColumns.Add(aInput[0].IndexOf('S'));
+aInput.RemoveAt(0);
+
+ulong aTotalCollissions = 0;
+foreach (var aRow in aInput)
+{
+    var aSplitterColumns = GetSplitterColumns(aRow);
+
+    foreach(var aColumn in aSplitterColumns)
+    {
+        // wenn splitter in BeamColumns ist, dann
+        if (aBeamColumns.Contains(aColumn))
+        {
+            // Kollissionen hochzählen
+            aTotalCollissions++;
+
+            // Originalsplitter entfernen
+            aBeamColumns.Remove(aColumn);
+
+            // aufteilen in zwei beams
+            // neuen beam nur hinzufügen, wenn er nicht schon existiert
+            if (!aBeamColumns.Contains(aColumn-1))
+                aBeamColumns.Add(aColumn-1);
+            if (!aBeamColumns.Contains(aColumn + 1))
+                aBeamColumns.Add(aColumn+1);
+        }
+    }
+
+}
+
+
+Console.WriteLine($"Total splits: {aTotalCollissions}");
+Console.WriteLine("[ENTER]");
+Console.ReadLine();
+
+
+
+List<int> GetSplitterColumns(string aRow)
+{
+    List<int> Result = new List<int>();
+    for (var aIndex = 0; aIndex < aRow.Length; aIndex++)
+    {
+        if (aRow[aIndex] == '^')
+        {
+            Result.Add(aIndex);
+        }
+    }
+    return Result;
+}
---
+++ Day7-Puzzle2/Program.cs
@@ -0,0 +1,65 @@
+﻿//var aInput = File.ReadLines("Input.txt").ToList().Where(aLine => aLine.Any(s => s == 'S' || s == '^')).ToList();
+var aInput = File.ReadLines("Sample.txt").ToList().Where(aLine => aLine.Any(s => s == 'S' || s == '^')).ToList();
+
+
+var aBeamColumns = new Dictionary<int, int>();
+aBeamColumns.Add(aInput[0].IndexOf('S'), 1);
+aInput.RemoveAt(0);
+var aBeamColumnList = new List<Dictionary<int, int>>();
+
+for (var aRowIndex = 0; aRowIndex < aInput.Count; aRowIndex++)
+{
+    var aRow = aInput[aRowIndex];
+    var aSplitterColumns = GetSplitterColumns(aRow);
+    Console.WriteLine($"Processing row {aRowIndex + 1} / {aInput.Count}, current paths: {aBeamColumns.Values.Sum()} - Splitter columns: {aSplitterColumns.Count}");
+
+    var aColumns = new List<int>(aBeamColumns.Keys);
+    //aBeamColumnList.Add(aColumns);
+
+    foreach (var aBeamColumn in aColumns)
+    {
+        if (aSplitterColumns.Contains(aBeamColumn))
+        {
+            // Originalbeam entfernen
+            aBeamColumns[aBeamColumn] = aBeamColumns[aBeamColumn] - 1;
+            if(aBeamColumns[aBeamColumn] == 0)
+                aBeamColumns.Remove(aBeamColumn);
+
+            if (!aBeamColumns.ContainsKey(aBeamColumn - 1))
+                aBeamColumns.Add(aBeamColumn - 1, 1);
+            else
+                aBeamColumns[aBeamColumn - 1] = aBeamColumns[aBeamColumn - 1] * 2;
+
+            if (!aBeamColumns.ContainsKey(aBeamColumn + 1))
+                aBeamColumns.Add(aBeamColumn + 1, 1);
+            else
+                aBeamColumns[aBeamColumn + 1] = aBeamColumns[aBeamColumn + 1] * 2;
+        }
+    }
+}
+
+//var aSum = 0;
+//foreach (var aBeamColumnListEntry in aBeamColumnList)
+//{
+//    aSum += aBeamColumnListEntry.Values.Sum();
+//}
+
+
+Console.WriteLine($"Total paths: {aBeamColumns.Values.Sum()}");
+Console.WriteLine("[ENTER]");
+Console.ReadLine();
+
+
+
+List<int> GetSplitterColumns(string aRow)
+{
+    List<int> Result = new List<int>();
+    for (var aIndex = 0; aIndex < aRow.Length; aIndex++)
+    {
+        if (aRow[aIndex] == '^')
+        {
+            Result.Add(aIndex);
+        }
+    }
+    return Result;
+}
